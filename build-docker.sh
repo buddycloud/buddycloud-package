d4d89eb85605326e9fedce50e4d671e5b5030072
rodrigods <rodrigodsousa@gmail.com>
2014-01-28 10:27:26 +0100

    Run tigase docker script

--- build-docker.sh
+++ build-docker.sh
@@ -4,7 +4,10 @@ PACKAGE_PROJECT=
 LATEST_DEB=$(ls -t *.deb | head -n 1)
 DEPLOY_USER_AT_HOST=$DEPLOY_USER@$DEPLOY_HOST
 
+# Get latest changes for buddycloud-package
 ssh $DEPLOY_USER_AT_HOST "cd $PACKAGE_PROJECT; git pull"
+
+# Docker bits folder
 DOCKER_PROJECT=$PACKAGE_PROJECT/projects/$1/docker
 
 scp $LATEST_DEB $DEPLOY_USER_AT_HOST:$DOCKER_PROJECT/$1.deb
