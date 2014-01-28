f86564d860d6ad71954ccf4cc3be4219e1e4b67d
rodrigods <rodrigodsousa@gmail.com>
2014-01-28 12:27:47 +0100

    Is deb project flag

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT_NAME=$1
-LATEST_DEB=$(ls -t *.deb | head -n 1)
+IS_DEB_PROJECT=$2
 DEPLOY_USER_AT_HOST=$DEPLOY_DOCKER_USER@$DEPLOY_DOCKER_HOST
 CURRENT_DIR="$(cd $DIR_NAME; pwd)"
 
@@ -15,7 +15,10 @@ ssh $DEPLOY_USER_AT_HOST "cd $PACKAGE_PROJECT; git pull"
 DOCKER_PROJECT=$PACKAGE_PROJECT/projects/$PROJECT_NAME/docker
 
 # Copy latest DEB file
-scp $LATEST_DEB $DEPLOY_USER_AT_HOST:$DOCKER_PROJECT/$PROJECT_NAME.deb
+if $IS_DEB_PROJECT; then
+  LATEST_DEB=$(ls -t *.deb | head -n 1)
+  scp $LATEST_DEB $DEPLOY_USER_AT_HOST:$DOCKER_PROJECT/$PROJECT_NAME.deb
+fi
 
 # Build docker
 ssh $DEPLOY_USER_AT_HOST "cd $DOCKER_PROJECT; docker build ."
