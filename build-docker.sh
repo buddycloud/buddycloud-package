a7cb98bffe3c22e8ef45c17a3ae289ea3e5cb954
rodrigods <rodrigodsousa@gmail.com>
2014-01-28 11:42:00 +0100

    Remove unecessary &

--- build-docker.sh
+++ build-docker.sh
@@ -1,15 +1,23 @@
-DEPLOY_USER=
-DEPLOY_HOST=
-PACKAGE_PROJECT=
+#!/bin/bash
+
+PROJECT_NAME=$1
+DEPLOY_USER=$2
+DEPLOY_HOST=$3
+PACKAGE_PROJECT=$4
 LATEST_DEB=$(ls -t *.deb | head -n 1)
 DEPLOY_USER_AT_HOST=$DEPLOY_USER@$DEPLOY_HOST
 
 # Get latest changes for buddycloud-package
 ssh $DEPLOY_USER_AT_HOST "cd $PACKAGE_PROJECT; git pull"
 
-# Docker bits folder
-DOCKER_PROJECT=$PACKAGE_PROJECT/projects/$1/docker
+# Docker folder
+DOCKER_PROJECT=$PACKAGE_PROJECT/projects/$PROJECT_NAME/docker
 
-scp $LATEST_DEB $DEPLOY_USER_AT_HOST:$DOCKER_PROJECT/$1.deb
+# Copy latest DEB file
+scp $LATEST_DEB $DEPLOY_USER_AT_HOST:$DOCKER_PROJECT/$PROJECT_NAME.deb
+
+# Build docker
 ssh $DEPLOY_USER_AT_HOST "cd $DOCKER_PROJECT; docker build ."
-ssh $DEPLOY_USER_AT_HOST "cd $DOCKER_PROJECT; ./run_docker $1"
+
+# Run
+ssh $DEPLOY_USER_AT_HOST "cd $DOCKER_PROJECT; ./run_docker $PROJECT_NAME"
