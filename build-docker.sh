e686169e13c6189a8a665be8ff1323fd3b47e6af
rodrigods <rodrigodsousa@gmail.com>
2014-01-28 12:34:00 +0100

    Right order'

--- build-docker.sh
+++ build-docker.sh
@@ -2,11 +2,12 @@
 
 PROJECT_NAME=$1
 IS_DEB_PROJECT=$2
-DEPLOY_USER_AT_HOST=$DEPLOY_DOCKER_USER@$DEPLOY_DOCKER_HOST
 CURRENT_DIR="$(cd $DIR_NAME; pwd)"
 
 # Source config
 source "$CURRENT_DIR/settings.production"
+
+DEPLOY_USER_AT_HOST=$DEPLOY_DOCKER_USER@$DEPLOY_DOCKER_HOST
 
 # Get latest changes for buddycloud-package
 ssh $DEPLOY_USER_AT_HOST "cd $PACKAGE_PROJECT; git pull"
