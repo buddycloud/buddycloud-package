3f1084fa48d01e6a80d3c10dc2acf424d31dede2
rodrigods <rodrigodsousa@gmail.com>
2014-01-28 18:14:17 +0100

    Get logs output

--- stop-and-run-docker.sh
+++ stop-and-run-docker.sh
@@ -27,6 +27,7 @@ ssh $DEPLOY_USER_AT_HOST "docker ps | grep $PROJECT_NAME:latest"
 RUN_CHECK=$?
 
 if [ $RUN_CHECK == 1 ]; then
-  ssh $DEPLOY_USER_AT_HOST "docker ps -a | grep $PROJECT_NAME:latest | head -n1 | cut -d' ' -f1"
+  CONTAINER=$(ssh $DEPLOY_USER_AT_HOST "docker ps -a | grep $PROJECT_NAME:latest | head -n1 | cut -d' ' -f1")
+  ssh $DEPLOY_USER_AT_HOST "docker logs $CONTAINER"  
   exit 1
 fi
