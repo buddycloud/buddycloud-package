9981b89263ddbe2f9f4f474dff76326f41d4fe5e
root <root@si.buddycloud.com>
2014-02-05 13:38:46 +0000

    Redirecting tigase's log to container output. Using anon.buddycloud.com as a single anon domain.

--- projects/buddycloud-http-api/docker/config.js
+++ projects/buddycloud-http-api/docker/config.js
@@ -7,6 +7,7 @@ exports.production = {
   debug: true,
   xmppHost: '#TIGASE_HOST#',
   channelDomain: 'channels.buddycloud.net',
+  xmppAnonymousDomain: 'anon.buddycloud.com',
   pusherComponent: 'pusher.buddycloud.com',
   friendFinderComponent: 'friend-finder.buddycloud.com',
   searchComponent: 'search.buddycloud.org',
