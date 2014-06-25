f82da8902d4c8407794dc591288ece954bdbe1ab
Andre Roesner <roesner.andre@web.de>
2014-06-25 02:12:23 +0200

    typo in server address

--- projects/webclient/docker/run_ls_fw.sh
+++ projects/webclient/docker/run_ls_fw.sh
@@ -3,7 +3,7 @@
 cat << EOF > /tmp/config.json
 {
  "network": {
-	"servers": ["abyss.budycloud.com:31337"],
+	"servers": ["abyss.buddycloud.com:31337"],
         "ssl certificate": "/opt/logstash-forwarder/logstash-forwarder.crt",
         "ssl key": "/opt/logstash-forwarder/logstash-forwarder.key",
         "ssl ca": "/opt/logstash-forwarder/logstash-forwarder.crt",
