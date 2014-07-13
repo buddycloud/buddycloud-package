dca17b76f7a945c3f283cd620c4764c83f36c781
Andre Roesner <roesner.andre@web.de>
2014-07-13 17:00:11 +0200

    rewrote run_ls_fw.sh and log.list for an easy configuration of the logfiles an types

--- projects/webclient/docker/run_ls_fw.sh
+++ projects/webclient/docker/run_ls_fw.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-cat << EOF > /tmp/config.json
-{
- "network": {
-	"servers": ["abyss.buddycloud.com:31337"],
+OUTPUT=/tmp/config.json
+config=( $(<log.list) )
+i=0
+
+
+
+#generate cofiguration with file list based on log.list. but keeping one file fix to not corrupt the config
+echo -en '
+{										
+ "network": {								
+	"servers": ["abyss.buddycloud.com:31337"],		
         "ssl certificate": "/opt/logstash-forwarder/logstash-forwarder.crt",
-        "ssl key": "/opt/logstash-forwarder/logstash-forwarder.key",
+        "ssl key": "/opt/logstash-forwarder/logstash-forwarder.key",	
         "ssl ca": "/opt/logstash-forwarder/logstash-forwarder.crt",
-        "timeout": 15
-  },
-  "files": [
-    {
-      "paths": [ "/var/log/apache2/web.buddycloud.com-access.log" ],
-      "fields": { "type": "web_apache_access" }
-    },
-    {
-      "paths": [ "/var/log/apache2/other_vhosts_access.log" ],
-      "fields": { "type": "other_vhosts_apache_access" }
-    },
-    {
-      "paths": [ "/var/log/apache2/hosting.buddycloud.com-access.log" ],
-      "fields": { "type": "hosting_apache_access" }
-    },
-    {
-      "paths": [ "/var/log/apache2/xmpp-ftw.buddycloud.com-access.log" ],
-      "fields": { "type": "xmpp-ftw_apache_access" }
-    },
+        "timeout": 15						
+  },							
+  "files": [' > $OUTPUT
+
+while [ $((i+1)) -lt ${#config[@]} ]; do
+
+  echo -en '\n\t{
+	"paths": [ "'"${config[i]}"'" ],
+	"fields": { "type": "'"${config[$((i+1))]}"'" }
+	},' >> $OUTPUT
+  i=$((i+2))
+done
+echo -en '
     {
       "paths": [ "/var/log/apache2/error.log" ],
       "fields": { "type": "general_apache_error" }
     }
   ]
-}
-EOF
+}' >> $OUTPUT
+
 
 /opt/logstash-forwarder/bin/logstash-forwarder -config /tmp/config.json &
 
