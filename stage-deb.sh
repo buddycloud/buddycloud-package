b27b76f2641a1eb3a39cc36eaa1e5e1c1f2f6608
Abmar Barros <abmargb@gmail.com>
2014-08-11 11:56:50 -0300

    stage-deb: getting most recent .dsc file

--- stage-deb.sh
+++ stage-deb.sh
@@ -4,9 +4,9 @@ P_USER=$1
 P_HOST=$2
 P_TAG=$3
 
-RELEASE=$(ls *.dsc | rev | cut -d'.' -f2- | rev)
+RELEASE=$(ls -t *.dsc | head -1 | rev | cut -d'.' -f2- | rev)
 PACKAGE=$(echo $RELEASE | cut -d'_' -f1)
 TARGET_DIR="/var/web/downloads.buddycloud.com/packages/debian/$P_TAG/$PACKAGE/$RELEASE"
 
-ssh $P_USER@$P_HOST -c "mkdir -p $TARGET_DIR"
+ssh $P_USER@$P_HOST mkdir -p $TARGET_DIR
 scp $RELEASE* $P_USER@$P_HOST:$TARGET_DIR
