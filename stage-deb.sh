e2b4bd22909c7861d93716705bdf9435f704755f
Abmar Barros <abmargb@gmail.com>
2014-08-11 11:33:34 -0300

    Create stage-deb.sh

---
+++ stage-deb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+P_USER=$1
+P_HOST=$2
+P_TAG=$3
+
+RELEASE=$(ls *.dsc | rev | cut -d'.' -f2- | rev)
+PACKAGE=$(echo $RELEASE | cut -d'_' -f1)
+TARGET_DIR="/var/web/downloads.buddycloud.com/packages/debian/$P_TAG/$PACKAGE/$RELEASE"
+
+ssh $P_USER@$P_HOST -c "mkdir -p $TARGET_DIR"
+scp $RELEASE* $P_USER@$P_HOST:$TARGET_DIR
