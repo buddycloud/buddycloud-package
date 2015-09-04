fb9d9b88fd58b58e96966188a18e1dda65f52722
root <root@buddycloud.com>
2015-09-04 09:57:21 +0000

    many small changes.

--- build-docker.sh
+++ build-docker.sh
@@ -1,12 +1,38 @@
-DEPLOY_USER=
-DEPLOY_HOST=
-PACKAGE_PROJECT=
-LATEST_DEB=$(ls -t *.deb | head -n 1)
-DEPLOY_USER_AT_HOST=$DEPLOY_USER@$DEPLOY_HOST
+#!/bin/bash
+set -x
 
+PROJECT_NAME=$1
+BUILD_ENV=$2
+STAGE_DEB=$3
+DIR_NAME="$(dirname $0)"
+CURRENT_DIR="$(cd $DIR_NAME; pwd)"
+
+# Source config
+source "$CURRENT_DIR/settings.$BUILD_ENV"
+
+DEPLOY_USER_AT_HOST=$DEPLOY_DOCKER_USER@$DEPLOY_DOCKER_HOST
+
+# Get latest changes for buddycloud-package
 ssh $DEPLOY_USER_AT_HOST "cd $PACKAGE_PROJECT; git pull"
-DOCKER_PROJECT=$PACKAGE_PROJECT/projects/$1/docker
 
-scp $LATEST_DEB $DEPLOY_USER_AT_HOST:$DOCKER_PROJECT/$1.deb
-ssh $DEPLOY_USER_AT_HOST "cd $DOCKER_PROJECT; docker build ."
-ssh $DEPLOY_USER_AT_HOST "cd $DOCKER_PROJECT; ./run_docker $1"
+# Docker folder
+DOCKER_PROJECT=$PACKAGE_PROJECT/projects/$PROJECT_NAME/docker
+
+# Replace variables
+DOCKER_PROJECT_HOST=$CURRENT_DIR/projects/$PROJECT_NAME/docker
+bash $CURRENT_DIR/replace-vars.sh $CONF_FOLDER $DOCKER_PROJECT_HOST
+for TPL_FILE in $DOCKER_PROJECT_HOST/*.tpl; do
+  CONF_FILE="${TPL_FILE%.*}"
+  CONF_FILE_NAME="$(basename $CONF_FILE)"
+  scp $CONF_FILE $DEPLOY_USER_AT_HOST:$DOCKER_PROJECT/$CONF_FILE_NAME
+done
+
+# Copy latest DEB file
+if $STAGE_DEB; then
+  LATEST_DEB=$(ls -t *.deb | head -n 1)
+  scp $LATEST_DEB $DEPLOY_USER_AT_HOST:$DOCKER_PROJECT/$PROJECT_NAME.deb
+fi
+
+# Build docker
+ssh $DEPLOY_USER_AT_HOST "cd $DOCKER_PROJECT; docker build -t $PROJECT_NAME ."
+
